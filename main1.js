589bd5fa7e60c7d35d57b002a171e73d76231417
HojinMoon <phantomhojin@naver.com>
2022-04-21 16:16:58 +0900

    LOGO plus

---
+++ main1.js
@@ -0,0 +1,15 @@
+const J1 = document.getElementById("J1")
+const I1 = document.getElementById("I1")
+const N1 = document.getElementById("N1")
+
+const J2 = document.getElementById("J2")
+const I2 = document.getElementById("I2")
+const N2 = document.getElementById("N2")
+
+J1.innerHTML = "J"
+I1.innerHTML = "I"
+N1.innerHTML = "N"
+
+J2.innerHTML = "J"
+I2.innerHTML = "I"
+N2.innerHTML = "N"
